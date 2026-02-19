acef12189807a03f684998f19d38ad148f439b18
Akhilachatti <cakhila2004@gmail.com>
2026-02-19 21:35:14 +0530

    Deploy in intial

--- Frontend/src/services/api.ts
+++ Frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://thinkplus-backend-w88k.onrender.com/api",
-  // baseURL:"http://localhost:5000/api",
+  //baseURL:"http://localhost:5000/api",
 });
 
 API.interceptors.request.use((config) => {
